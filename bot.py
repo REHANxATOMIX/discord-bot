8663ef4929dfe6fbe53bc4f0d75dace262e0ca1f
Mohammad Rehan Shaikh <149598343+REHANxATOMIX@users.noreply.github.com>
2024-08-25 12:12:58 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,19 @@ import random
 from collections import defaultdict
 import os
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
 # Initialize the bot with the command prefix
 intents = discord.Intents.default()
 intents.message_content = True
